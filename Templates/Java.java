6b011944456f5fa8f28758abff1a1a0456aea37e
skyflaren <45515570+waba359@users.noreply.github.com>
2019-12-28 13:58:15 -0500

    Update Java.java

--- Templates/Java.java
+++ Templates/Java.java
@@ -6,14 +6,14 @@ public class Main {
     static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
     static String next () throws IOException { while (st == null || !st.hasMoreTokens())
-	    st = new StringTokenizer(br.readLine().trim());return st.nextToken(); 
-    }
+	st = new StringTokenizer(br.readLine().trim());return st.nextToken(); }
     static long rlong () throws IOException { return Long.parseLong(next());}
     static int rint () throws IOException { return Integer.parseInt(next());}
     static double rdouble () throws IOException { return Double.parseDouble(next());}
     static String rline () throws IOException { return br.readLine().trim(); }
     static void outln(String a){ System.out.println(a); }
     static void outln(long a){ System.out.println(a); }
+    static void outln(double a){ System.out.println(a); }
     static void outln(long a, long b){ System.out.println(a + "  " + b); }
 
     public static void main(String [] args) throws IOException{
