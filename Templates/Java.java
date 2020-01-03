f0d8337c1f144cf8d7918c57add804de42824abd
skyflaren <45515570+waba359@users.noreply.github.com>
2020-01-02 23:33:44 -0500

    Update Java.java

--- Templates/Java.java
+++ Templates/Java.java
@@ -6,18 +6,21 @@ public class Main {
     static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
     static String next () throws IOException { while (st == null || !st.hasMoreTokens())
-	st = new StringTokenizer(br.readLine().trim());return st.nextToken(); }
+	    st = new StringTokenizer(br.readLine().trim());return st.nextToken(); }
     static long rlong () throws IOException { return Long.parseLong(next());}
     static int rint () throws IOException { return Integer.parseInt(next());}
     static double rdouble () throws IOException { return Double.parseDouble(next());}
     static String rline () throws IOException { return br.readLine().trim(); }
-    static void outln(String a){ System.out.println(a); }
-    static void outln(long a){ System.out.println(a); }
-    static void outln(double a){ System.out.println(a); }
-    static void outln(long a, long b){ System.out.println(a + "  " + b); }
+    static void out(String a){ pr.print(a); }
+    static void out(long a){ pr.print(a); }
+    static void outln(String a){ pr.println(a); }
+    static void outln(long a){ pr.println(a); }
+    static void outln(double a){ pr.println(a); }
+    static void outln(long a, long b){ pr.println(a + "  " + b); }
 
     public static void main(String [] args) throws IOException{
         int n = rint();
         
+        pr.close();
     }
 }
