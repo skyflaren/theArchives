e776fd65ca8664a9a53a72efd894fd6aca141924
Justin Lu <45515570+skyflaren@users.noreply.github.com>
2020-04-22 18:55:33 -0400

    Update segment-tree.cpp

--- Algorithmns/segment-tree.cpp
+++ Algorithmns/segment-tree.cpp
@@ -11,15 +11,18 @@ typedef unsigned long long ull;
 const int INF = 0x3F3F3F3F;
 const int MM = 1e5 + 5;
 
+#define lc (2*id)
+#define rc (2*id+1)
+
 struct node{ int l, r, v, g, f; } seg[3*MM];    //left, right, value, gcd, flag
 int n, m, arr[MM], a; char op;
 
 void push_up(int id){
-    seg[id].v = min(seg[2*id].v, seg[2*id+1].v);
-    seg[id].g = __gcd(seg[2*id].g, seg[2*id+1].g);
+    seg[id].v = min(seg[lc].v, seg[rc].v);
+    seg[id].g = __gcd(seg[lc].g, seg[rc].g);
     seg[id].f = 0;
-    if(seg[id].g == seg[2*id].g) seg[id].f += seg[2*id].f;
-    if(seg[id].g == seg[2*id+1].g) seg[id].f += seg[2*id+1].f;
+    if(seg[id].g == seg[lc].g) seg[id].f += seg[lc].f;
+    if(seg[id].g == seg[rc].g) seg[id].f += seg[rc].f;
 }
 
 void build(int l, int r, int id){
@@ -29,7 +32,7 @@ void build(int l, int r, int id){
         return; 
     }
     int mid = (seg[id].l + seg[id].r)/2;
-    build(l, mid, 2*id); build(mid+1, r, 2*id+1); 
+    build(l, mid, lc); build(mid+1, r, rc); 
     push_up(id);
 }
 
@@ -38,33 +41,33 @@ void update(int pos, int val, int id){
         seg[id].v = seg[id].g = val; return;
     }
     int mid = (seg[id].l + seg[id].r)/2;
-    if(pos <= mid) update(pos, val, 2*id);
-    else update(pos, val, 2*id+1);
+    if(pos <= mid) update(pos, val, lc);
+    else update(pos, val, rc);
     push_up(id); 
 }
 
 int qryMin(int l, int r, int id){
     if(seg[id].l == l && seg[id].r == r) return seg[id].v;
     int mid = (seg[id].l + seg[id].r)/2;
-    if(r <= mid) return qryMin(l, r, 2*id);
-    if(l > mid) return qryMin(l, r, 2*id+1);
-    else return min(qryMin(l, mid, 2*id), qryMin(mid+1, r, 2*id+1));
+    if(r <= mid) return qryMin(l, r, lc);
+    if(l > mid) return qryMin(l, r, rc);
+    else return min(qryMin(l, mid, lc), qryMin(mid+1, r, rc));
 }
 
 int qryGcd(int l, int r, int id){
     if(seg[id].l == l && seg[id].r == r) return seg[id].g;
     int mid = (seg[id].l + seg[id].r)/2;
-    if(r <= mid) return qryGcd(l, r, 2*id);
-    if(l > mid) return qryGcd(l, r, 2*id+1);
-    else return __gcd(qryGcd(l, mid, 2*id), qryGcd(mid+1, r, 2*id+1));
+    if(r <= mid) return qryGcd(l, r, lc);
+    if(l > mid) return qryGcd(l, r, rc);
+    else return __gcd(qryGcd(l, mid, lc), qryGcd(mid+1, r, rc));
 }
 
 int qryCnt(int l, int r, int g, int id){
     if(seg[id].l == l && seg[id].r == r) return seg[id].g == g? seg[id].f: 0;
     int mid = (seg[id].l + seg[id].r)/2;
-    if(r <= mid) return qryCnt(l, r, g, 2*id);
-    if(l > mid) return qryCnt(l, r, g, 2*id+1);
-    return qryCnt(l, mid, g, 2*id) + qryCnt(mid+1, r, g, 2*id+1);
+    if(r <= mid) return qryCnt(l, r, g, lc);
+    if(l > mid) return qryCnt(l, r, g, rc);
+    return qryCnt(l, mid, g, lc) + qryCnt(mid+1, r, g, rc);
 }
 
 
@@ -77,8 +80,8 @@ int main() {
     for(int i = 1, x, y; i <= m; i++){
         cin >> op >> x >> y;
         if(op == 'C') update(x, y, 1);
-        else if(op == 'M') cout << qryMin(x, y, 1) << "\n";
-        else if(op == 'G') cout << qryGcd(x, y, 1) << "\n";
-        else cout << qryCnt(x, y, qryGcd(x,y, 1), 1) << "\n";
+        else if(op == 'M') cout << qryMin(x, y, 1) << ln;
+        else if(op == 'G') cout << qryGcd(x, y, 1) << ln;
+        else cout << qryCnt(x, y, qryGcd(x,y, 1), 1) << ln;
     }
 }
